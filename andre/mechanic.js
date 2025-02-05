843f3970b01f1c679c45c2233609aeb5e6f03048
fake-user-dark <mcat40310@gmail.com>
2025-02-05 17:00:28 +0100

    all files

---
+++ andre/mechanic.js
@@ -0,0 +1,17 @@
+who_answerd = ""
+
+function after_clicking() {
+
+    const a = document.querySelector('input').value;
+    console.log(a);
+    document.getElementById('1').remove()
+
+    
+    
+
+
+
+}
+
+
+
